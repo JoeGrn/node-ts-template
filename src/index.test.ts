684b40b0de8976e93c3be7c781812777a6cd906f
Joseph Green <joseph.peter.green@gmail.com>
2021-08-10 08:40:11 +0100

    improved tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,15 @@
 import { add } from './index';
 
-it('adds two numbers together', () => {
-  expect(add(2, 2)).toEqual(4);
-});
+const validInputs = [
+    { input1: 1, input2: 1, expected: 2 },
+    { input1: 0, input2: 2, expected: 2 },
+    { input1: 10, input2: 10, expected: 20 },
+    { input1: 999, input2: 999, expected: 1998 },
+    { input1: 1, input2: 1, expected: 2 }
+]
+
+describe.each(validInputs)('when add is given two valid numeric inputs', ({ input1, input2, expected }) => {
+    it('should return the addition of the two inputs', () => {
+        expect(add(input1, input2)).toEqual(expected);
+    });
+})
